888a6fa5241be1f065ad50327f5b3f85529a1f80
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-13 20:26:41 +0530

    create Nabvar after login, the navbar displays name, profile picture and logout button to logout

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = ()=>{
+    return(
+        <>
+        <div className="navbar">
+        <span className="logo">Time Pass</span>
+        <div className="user">
+            <img src="" alt=""/>
+            <button>Logout</button>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
