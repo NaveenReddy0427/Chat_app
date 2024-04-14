a9afc532a9ab733f0ae66393620e9af811d61df7
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-14 09:01:00 +0530

    config the auth to the firbase and for the login page, if user account is already there, if he logins with correct credentials, loggin is successful and redirect to home page using navigate element which is there in React

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,26 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from "firebase"
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () =>{
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [err, setErr] = useState(false)
+    const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/")
+          } catch (err) {
+            setErr(true);
+          }
     }
 
     return(
@@ -13,9 +30,16 @@ const Login = () =>{
                 <span className="logo">Friend Chat</span>
                 <span className="title">Login</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
+
+                    <input type="email" placeholder="Enter your email here" 
+                    value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                    <input type="password" placeholder="Enter your password here"  
+                    value={password} onChange={(e) => setPassword(e.target.value)} />
+
                     <button>Sign in</button>
+                    {err && <span className="error-message">Something went wrong</span>}
+                    
                     </form>
                 <p>You don't have an account? <Link to="/register">Register</Link></p>
             </div>
