7016d4674aa0fcc8d156dd3547dd9be633b17188
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-14 08:40:41 +0530

    install firebase to store and retrieve the data, and create firebase file and embed the config code as well

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp} from "firebase/app"
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrAswYnnQJNlVwOo4gohLfI3migJLNUvg",
+    authDomain: "chat-application-dcf81.firebaseapp.com",
+    projectId: "chat-application-dcf81",
+    storageBucket: "chat-application-dcf81.appspot.com",
+    messagingSenderId: "891556867342",
+    appId: "1:891556867342:web:c646dca87082e083ae3b5f"
+};
+
+
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth();
